e8e9d05acaff5b9f7e7bc6b3ef93c572f4867dc6
Nicolás Vásquez <conceptorobledo@gmail.com>
2018-07-17 11:25:23 -0400

    fire on child_added

--- src/actions/dataAction.js
+++ src/actions/dataAction.js
@@ -4,27 +4,24 @@ import { patrolsRef, homesRef } from '../firebase';
 
 export const fetchPatrols = () => async dispatch => {
     patrolsRef.limitToLast(10).on("child_added", snapshot => {
-        if (snapshot.val() == null) {
+        const singlePatrol = snapshot.val();
+        if (singlePatrol == null) {
             return false;
         }
-        console.log(snapshot.val());
-        snapshot.forEach(patrol => {
-            const singlePatrol = patrol.val();
-            homesRef.child(singlePatrol.homeId).once('value', snap => {
-                const singleHome = snap.val();
-                const payload = {
-                    address: singleHome.address,
-                    status: singlePatrol.status,
-                    timestamp: singlePatrol.timestamp,
-                    key: patrol.key
-                }
-                dispatch({
-                    type: FETCH_PATROLS,
-                    payload: payload
-                });
+        homesRef.child(singlePatrol.homeId).once('value', snap => {
+            const singleHome = snap.val();
+            const payload = {
+                address: singleHome.address,
+                status: singlePatrol.status,
+                timestamp: singlePatrol.timestamp,
+                key: snapshot.key
+            }
+            dispatch({
+                type: FETCH_PATROLS,
+                payload: payload
             });
         });
     });
 }
 
-export const fetchHomes = () => {}
+export const fetchHomes = () => { }
--- src/reducers/dataReducer.js
+++ src/reducers/dataReducer.js
@@ -1,25 +1,15 @@
 import { FETCH_PATROLS } from '../actions/types';
 
 const initialState = {
-    patrols: []
+    patrols:[]
 }
 
 export default function (state = initialState, action) {
     switch (action.type) {
         case FETCH_PATROLS:
-            const duplicate = state.patrols.some(o => {
-                return o.key === action.payload.key
-            });
-            if (duplicate) {
-                return state;
-            }
-            else {
-                const newState = [...state.patrols, action.payload];
-                newState.sort(function (a, b) { return b.timestamp - a.timestamp });
-                return {
-                    patrols: newState.slice(0,10)
-                };
-            }
+            return {
+                patrols: [...state.patrols,action.payload]
+            };
         default:
             return state;
     }
