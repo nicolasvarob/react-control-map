9fe060483cd1cc9602fcd2cf6c359e0d881a53bf
Nicolás Vásquez <conceptorobledo@gmail.com>
2018-08-09 15:33:57 -0400

    provider a index

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
 import MainMap from './views/MainMap';
 import Reporting from './views/Reporting';
 
-import store from './store';
 
 import './App.css'
 
 class App extends Component {
 
+  state={
+    loading:false
+  }
+
   render() {
 
     return (
-      <Provider store={store}>
         <div className="container-fluid">
           <Switch>
             <Route path="/" component={MainMap} exact />
             <Route path="/reporting" component={Reporting} />
           </Switch>
         </div>
-      </Provider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './store';
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
     (<BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>)
     , document.getElementById('root'));
 registerServiceWorker();
